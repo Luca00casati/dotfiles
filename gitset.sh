d47e9e253c294cf2f2bfe5800ce51ae977cd8f2b
Luca Casati <luk00casati@gmail.com>
2025-01-30 00:31:29 +0100

    git

--- gitset.sh
+++ gitset.sh
@@ -2,3 +2,4 @@
 set -e
 git config --global user.name "Luca Casati"
 git config --global user.email "luk00casati@gmail.com"
+git config --global url.ssh://git@github.com/.insteadOf https://github.com/
