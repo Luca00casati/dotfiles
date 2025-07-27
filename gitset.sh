e2ea7d4d7077133d74e2ac1367df5f90b7a356bd
Luca Casati <luk00casati@gmail.com>
2025-07-27 15:37:29 +0200

    hyprland

--- gitset.sh
+++ gitset.sh
@@ -2,4 +2,3 @@
 set -e
 git config --global user.name "Luca Casati"
 git config --global user.email "luk00casati@gmail.com"
-#git config --global url.ssh://git@github.com/.insteadOf https://github.com/
