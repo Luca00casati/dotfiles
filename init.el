d343068c6278f6c139e09069630da5a16d8cad44
Luca Casati <luk00casati@gmail.com>
2025-09-19 22:48:33 +0200

    new

--- init.el
+++ init.el
@@ -1,30 +1,35 @@
-(setq inhibit-startup-message t)
-(menu-bar-mode -1)  
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-;; Display line numbers in every buffer
-(global-display-line-numbers-mode 1)
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
-;;melpa
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; use MELPA to package archives
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")))
+
+;; Initialize the package system
 (package-initialize)
 
-(use-package doom-themes
-  :ensure t
-  :config
-  (load-theme 'doom-one t))
+;; Refresh package list if needed
+(unless package-archive-contents
+  (package-refresh-contents))
 
-(use-package doom-modeline
-  :ensure t
-  :hook (after-init . doom-modeline-mode))
+;;helper
+(defun usep (pkg)
+(unless (package-installed-p pkg)
+  (package-install pkg)))
 
-(which-key-mode)
-;;(electric-pair-mode 1)
-(global-hl-line-mode 1)
-(savehist-mode 1)
-(save-place-mode 1)
+(usep 'gruber-darker-theme)
 
 (setq auto-save-default nil)
-(setq backup-directory-alist '(("." . "~/.config/emacs/backup")))
+(setq make-backup-files nil)
+(setq inhibit-splash-screen t)
 
+(setq initial-scratch-message ";scratch\n")
+
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(global-display-line-numbers-mode)
+(savehist-mode 1)
+(save-place-mode 1)
