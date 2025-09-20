06d958bc92b0d78d9ac653758734075d8b9887d9
Luca Casati <luk00casati@gmail.com>
2025-09-20 15:47:08 +0200

    ivy

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 (setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
+(load custom-file t t)
 
 (require 'package)
 
@@ -13,17 +13,21 @@
 ;; Refresh package list if needed
 (unless package-archive-contents
   (package-refresh-contents))
+;;(package-refresh-contents)
 
 ;;helper
 (defun usep (pkg)
 (unless (package-installed-p pkg)
   (package-install pkg)))
 
+;;theme
 (usep 'gruber-darker-theme)
+(load-theme 'gruber-darker)
 
 (setq auto-save-default nil)
 (setq make-backup-files nil)
 (setq inhibit-splash-screen t)
+(setq use-dialog-box nil)
 
 (setq initial-scratch-message ";scratch\n")
 
@@ -33,3 +37,32 @@
 (global-display-line-numbers-mode)
 (savehist-mode 1)
 (save-place-mode 1)
+
+;;packages
+(usep 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+(usep 'which-key)
+
+(usep 'counsel)
+(usep 'swiper)
+(usep 'ivy)
+
+(setq ivy-use-virtual-buffers t
+      ivy-count-format "%d/%d "
+      ivy-initial-input nil
+      ivy-wrap t
+      ivy-height 15)
+
+(ivy-mode)
+
+(setq swiper-isearch-full-regex nil)
+(setq counsel-M-x-initial-input "^")
+
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "C-r") 'swiper)
+(global-set-key (kbd "C-x b") 'ivy-switch-buffer)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+
+(usep 'magit)
